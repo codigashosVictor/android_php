7f35aee0fa208a6695840b8cd0c72fe93882b28c
codigashosVictor <victor_1997_sid@hotmail.com>
2017-03-29 11:08:46 -0700

    no se que pedo

--- get_user.php
+++ get_user.php
@@ -1,16 +1,22 @@
 <?php
-	require_once('models/User.php');
+	require_once('models/user.php');
 	require_once('security.php');
 	header('Access-Control-Allow-Origin:*');
-	header('Access-Control-Allow-Headers: username, password');
-	$header = getallheaders();
 
-	if (isset($header['username']) & isset($header['password']))
+	header('Access-Control-Allow-Headers: username, password');
+	$header = getallheaders(); 
+
+	if (isset($header['username']) && isset($header['password']))
 	{
-		try 
+		//create user
+		$u = new User($header['username'], $header['password']);
+
+		if ($u->get_id_user() == '')
 		{
-			//create user
-			$u = new User($header['username'], $header['password']);
+			echo '{ "status" : 1, "errorMessage" : "User not Found" }';	
+		}
+		else
+		{
 			//display user
 			echo '{ "status" : 0,
 					"id" : "'.$u->get_id_user().'",
@@ -20,11 +26,8 @@
 					"email" : "'.$u->get_email().'",
 					"image" : "'.$u->get_imageURL().'",
 					"token" : "'.Security::generate_token($u->get_id_user()).'"
-				}';	
-		} catch (InvalidUserException $ex) 
-		{
-			echo '{ "status" : 1, "errorMessage" : "'.$ex->get_message().'" }';	
-		}
+				}';
+		}	
 	}
 	else
 		echo '{ "status" : 2, "errorMessage" : "Invalid Parameters" }';
